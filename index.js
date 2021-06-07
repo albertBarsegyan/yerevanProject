ba352cd8d2ddb00feba25b0cf3ef209f96ca7c92
Albert <albertbarsegyan6@gmail.com>
2021-06-07 15:21:47 +0400

    docs(index.html) add loader

---
+++ index.js
@@ -0,0 +1,18 @@
+const loaderContainer = document.querySelector('.loader');
+const mainContainer = document.querySelector('.main-content');
+
+function stopLoader() {
+  setTimeout(() => {
+    setTimeout(() => {
+      loaderContainer.style.opacity = 0;
+    }, 1000);
+    setTimeout(() => {
+      loaderContainer.style.display = 'none';
+    }, 1500);
+    mainContainer.style.display = 'block';
+    setTimeout(() => {
+      mainContainer.style.opacity = 1;
+    }, 2000);
+  }, 3000);
+}
+stopLoader();
